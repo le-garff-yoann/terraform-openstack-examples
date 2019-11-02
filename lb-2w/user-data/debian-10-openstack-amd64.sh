0296cd529fbf375aa1c21d37e41e13b2c3e7cc68
le-garff-yoann <pe.weeble@yahoo.fr>
2019-11-02 20:46:37 +0100

    fix(user-data) apt with -y is way better...

--- lb-2w/user-data/debian-10-openstack-amd64.sh
+++ lb-2w/user-data/debian-10-openstack-amd64.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 sudo apt update
-sudo apt upgrade
+sudo apt -y upgrade
 
-sudo apt install apache2 apache2-utils
+sudo apt install -y apache2 apache2-utils
 
 sudo systemctl enable apache2
 sudo systemctl restart apache2
